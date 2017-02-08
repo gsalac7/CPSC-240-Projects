54fa278cf9597d0e01a417452675d1c4128cfc29
Gabe Salac <gsalac7@gmail.com>
2017-02-07 23:22:28 -0800

    Update Part4.cpp

--- Project 2/Part4.cpp
+++ Project 2/Part4.cpp
@@ -13,23 +13,23 @@ int main() {
 
 	cin >> d;
 
-	_asm {
-		mov eax, d;
-		mov ecx, 100;
-		cwd;
-		idiv ecx;
-		mov hund, eax;
-		mov eax, edx;
-		mov ecx, 10;
-		cwd;
-		idiv ecx;
-		mov ten, eax;
-		mov one, edx;
-		add edx, ten;
-		add edx, hund;
-		mov sum, edx;
+	_asm { // find sum of the digits
+		mov eax, d; // assign d to eax
+		mov ecx, 100; // assign 100 to ecx
+		cwd; // change to 64 bits
+		idiv ecx; // divide eax by ecx, d/100
+		mov hund, eax; // assign hund to eax
+		mov eax, edx; // assign edx to eax
+		mov ecx, 10; // assign 10 to ecx for division
+		cwd; // change to 64 bits
+		idiv ecx; // divide eax by ecx
+		mov ten, eax; // assign eax to ten
+		mov one, edx; // assign edx to one
+		add edx, ten; // add ten + edx
+		add edx, hund; // add hund + edx
+		mov sum, edx; // assign edx to sum
 	}
-	
+
 	cout << "\tThe total of the digits in " << d << " is " << sum << endl;
 	return 0;
 }
