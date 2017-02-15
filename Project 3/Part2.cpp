379c40aa46f0618f5ca7e43e352822f343b0c03c
Gabe Salac <gsalac7@gmail.com>
2017-02-14 21:08:18 -0800

    Update Part2.cpp

--- Project 3/Part2.cpp
+++ Project 3/Part2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-
 using namespace std;
+
 int counter = 1, score = 0, average;
 
 void readData() {
@@ -9,7 +9,7 @@ void readData() {
 }
 
 void displayAverage() {
-	cout << "Your average is " << average;
+	cout << "Your average is " << average << endl;
 }
 
 int main() {
@@ -17,22 +17,25 @@ int main() {
 		mov eax, score; //assign score to eax register
 		mov ebx, average;
 		mov ecx, counter;
+		call readData;
 	compare:
-		cmp score, -1;
-		jne readData;
-		sub ecx, 1;
-		add eax, 1;
+		cmp score, -1;		//compare the data just read(score) to - 1
+		jne readData;		//if the score is not equal to - 1, keep reading data.
+		add eax, score;	
+		sub ecx, 1;			//else if the scoer is equal to -1, then take 1 away from the counter.
+		add	score, 1;		// and since 1 was subtracted from the score, add 1.
+
 		jmp done;
 	readData:
 		call readData;
-		add ecx, 1;
-		jmp compare;
-		add eax, score;
+		add counter, 1;		//add 1 to the counter
+		add	eax, score;	//add the score to the same score register.
+		jmp compare;	//go back to comparng to see if the next read data is or isn't -1
 	done:
-		cqd;
-		idiv ecx;
-		mov ecx, average;
+		cdq;
+		idiv counter;
+		mov average, eax;
 		call displayAverage;
-
 	}
+	return 0;
 }
