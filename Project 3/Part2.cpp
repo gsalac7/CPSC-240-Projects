b87076ac1312c9da5446ae55abbd089e3cca1a0c
Gabe Salac <gsalac7@gmail.com>
2017-02-15 20:32:24 -0800

    Update Part2.cpp

--- Project 3/Part2.cpp
+++ Project 3/Part2.cpp
@@ -1,41 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int counter = 1, score = 0, average;
+int counter = 0, score = 0, average = 0, sum = 0;
 
 void readData() {
 	cout << "Enter your score (-1 to stop): ";
 	cin >> score;
 }
 
+void addData() {
+	sum += score;
+}
+
 void displayAverage() {
-	cout << "Your average is " << average << endl;
+	cout << "Your average is " << sum << endl;
 }
 
 int main() {
 	_asm {
-		mov eax, score; //assign score to eax register
-		mov ebx, average;
-		mov ecx, counter;
-		call readData;
-	compare:
-		cmp score, -1;		//compare the data just read(score) to - 1
-		jne readData;		//if the score is not equal to - 1, keep reading data.
-		add eax, score;	
-		sub ecx, 1;			//else if the scoer is equal to -1, then take 1 away from the counter.
-		add	score, 1;		// and since 1 was subtracted from the score, add 1.
+		mov		eax, sum; //assign sum to eax register
+		mov		ebx, counter; //assgn counter to ebx
 
-		jmp done;
-	readData:
-		call readData;
-		add counter, 1;		//add 1 to the counter
-		add	eax, score;	//add the score to the same score register.
-		jmp compare;	//go back to comparng to see if the next read data is or isn't -1
+	read:
+		call	readData; //call the readData function
+		call	addData;  //add score to sum...
+		add		ebx, 1;		//add 1 to the counter
+		
+	compare:
+		cmp		score, -1; //check if the score is -1
+		jne		read; //if score isn't -1 go to readdata again
+		sub		ebx, 1;	//subtract 1 from the counter since -1 doesnt count
+		add		eax, 1; //add 1 to the score to nullify the -1 added to the score
+
 	done:
 		cdq;
-		idiv counter;
-		mov average, eax;
-		call displayAverage;
+		idiv	ebx; //divide eax (sum) with ebx (counter) to get average in eax
+		mov		sum, eax; //move eax to average
+		call	displayAverage; //display the average
 	}
 	return 0;
 }
