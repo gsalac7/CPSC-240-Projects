0ebdf008809b6febdb49754e315afb1428ce0aca
Gabe Salac <gsalac7@gmail.com>
2017-02-20 17:10:20 -0800

    Update Part1.cpp

--- Project 4/Part1.cpp
+++ Project 4/Part1.cpp
@@ -1,3 +1,8 @@
+/*Angelo Salac
+Michael Busslinger
+2/23/17
+Project 4, Part 1
+*/
 #include <iostream>
 
 using namespace std;
@@ -14,35 +19,31 @@ void readData() {
 
 void yesVotes() {
 	cout << "No. of YES votes = " << yesCounter << " ";
-	_asm {
-		mov i, 0;
+	_asm { //loop to print the stars
+		mov		i, 0;
 	forloop:
-		call printStar;
-		inc i;
-		mov eax, i;
-		cmp eax, yesCounter;
-		jne forloop;
+		call	printStar; //print the star
+		inc		i; //increment i by 1
+		mov		eax, i;
+		cmp		eax, yesCounter; //compare i to the counter
+		jne		forloop; //if its not equal then keep repeating
 	done:
-
 	}
-
 	cout << endl;
 }
 
 void noVotes() {
 	cout << "No. of NO votes = " << noCounter << " ";
-	_asm {
-		mov i, 0;
+	_asm { //loop to print stars
+		mov		i, 0;
 	forloop:
-		call printStar;
-		inc i;
-		mov eax, i;
-		cmp eax, noCounter;
-		jne forloop;
+		call	printStar;  //print the star
+		inc		i;			//increment i by 1
+		mov		eax, i;		//move i to register
+		cmp		eax, noCounter;//check if i is counter
+		jne		forloop;	//if it is not, then repeat
 	done:
-
 	}
-
 	cout << endl;
 }
 
@@ -50,27 +51,27 @@ int main() {
 	cout << "Enter a string of votes: ";
 	_asm {
 	forloop:
-		call readData;
-		cmp c, 'y';
-		je addY;
-		jmp addN;
+		call	readData; //call read data
+		cmp		c, 'y'; //check to see if char is y
+		je		addY; //if it is, then add 1 to y
+		jmp		addN; //if it is not then add 1 to n
 	addY:
-		inc yesCounter;
-		inc i;
-		cmp i, 10;
-		je done;
-		jmp forloop;
+		inc		yesCounter; //add 1 to the ycounter
+		inc		i; //the vote counter
+		cmp		i, 10; //if its 10 votes, then its done
+		je		done;
+		jmp		forloop;
 
 	addN:
-		inc noCounter;
-		inc i;
-		cmp i, 10;
-		je done;
-		jmp forloop;
+		inc		noCounter; //add 1 to the ncounter
+		inc		i; //the vote counter
+		cmp		i, 10; //if its 10 votes, then its done
+		je		done;
+		jmp		forloop;
 
 	done:
-		call yesVotes;
-		call noVotes;
+		call	yesVotes; //display the yesvotes
+		call	noVotes; //display the no votes
 	}
 
 	return 0;
