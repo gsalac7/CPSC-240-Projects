08c4cf3746d9446e0fe4757d26771b06c4b85e48
Gabe Salac <gsalac7@gmail.com>
2017-02-20 17:05:45 -0800

    Update Part2.cpp

--- Project 4/Part2.cpp
+++ Project 4/Part2.cpp
@@ -1,13 +1,19 @@
+/*Angelo Salac
+Michael Busslinger
+2/23/16
+Project 4, Part2
+*/
 #include <iostream>
-
 using namespace std;
 
 char c;
-int vowels = 0;
+
+//function for reading char
 void readChar() {
 	cin.get(c);
 }
 
+//print char
 void printChar() {
 	cout << c;
 }
@@ -15,26 +21,27 @@ void printChar() {
 int main() {
 	cout << "Enter a sentence: ";
 	_asm {
-	whileloop:
-		call readChar;
-		cmp c, 'a';
-		je toupper;
-		cmp c, 'i';
-		je toupper;
-		cmp c, 'e';
-		je toupper;
-		cmp c, 'o';
-		je toupper;
-		cmp c, 'u';
-		je toupper;
-		cmp c, '\n';
-		je done;
-
-	toupper:
-		sub c, 32;
-		call printChar;
-		jmp whileloop;
-	done:
-
+	whileloop: //create a loop for reading the chars
+		call	readChar; //call read char function
+		cmp		c, '.'; //compare char with '.'
+		je		done; //if its equal then its done reading
+		cmp		c, 'a'; //compare the char with every vowel
+		je		ifV;
+		cmp		c, 'e';
+		je		ifV;
+		cmp		c, 'i';
+		je		ifV;
+		cmp		c, 'o';
+		je		ifV;
+		cmp		c, 'u';
+		je		ifV;
+		call	printChar; //print the character if it's not a vowel.
+		jmp		whileloop; //repeat
+	ifV:
+		sub		c, 32; //subtract the char to get the upper case vowels
+		call	printChar; //print the char
+		jmp		whileloop; //go back to the loop
+	done: //finished
 	}
+	cout << endl;
 }
