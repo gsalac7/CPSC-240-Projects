0d8bce55f06a63f9d3d9fbfb7470ca0deb02f379
Gabe Salac <gsalac7@gmail.com>
2017-02-20 17:18:06 -0800

    Update Part4.cpp

--- Project 4/Part4.cpp
+++ Project 4/Part4.cpp
@@ -1,3 +1,8 @@
+/*Angelo Salac
+Michael Busslinger
+2/23/17
+Project 4, Part 4
+*/
 #include<iostream>
 
 using namespace std;
@@ -22,20 +27,20 @@ void displayC() {
 int main() {
 	_asm { // convert farenheit to celsius
 	forloop:
-		call readData;
-		mov ax, f; // assign f to eax
-		mov bx, 5; // assign this for multipilying
-		mov cx, 9; // assign this for dividing
-		sub ax, 32; // subtract ax by 32, f - 32
-		imul bx; // multply ax by bx, 5(f - 32)
-		cwd; // change to 32 bits
-		idiv cx; // divide ax by cx, 5(f - 32)/9
-		mov c, ax; // assign ax to c
-		call displayC;
-		call continue1;
-		cmp choice, 'y';
-		je forloop;
-	done:
-
+		call	readData;
+		mov		ax, f; // assign f to eax
+		mov		bx, 5; // assign this for multipilying
+		mov		cx, 9; // assign this for dividing
+		sub		ax, 32; // subtract ax by 32, f - 32
+		imul	bx; // multply ax by bx, 5(f - 32)
+		cwd;		// change to 32 bits
+		idiv	cx; // divide ax by cx, 5(f - 32)/9
+		mov		c, ax; // assign ax to c
+		call	displayC; //display the Celsiuss
+		call	continue1; //ask to continue
+		cmp		choice, 'y'; //if yes the repeat the loop
+		je		forloop;
+	done: //else finished
 	}
+	return 0; 
 }
