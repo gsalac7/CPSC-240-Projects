34e28bc4368f5db67bd9734438c5c57bbc677506
Gabe Salac <gsalac7@gmail.com>
2017-02-28 23:41:40 +0000

    Update Part4.cpp

--- Project 5/Part4.cpp
+++ Project 5/Part4.cpp
@@ -1,57 +1,71 @@
 /*Angelo Salac
 Michael Busslinger
 3/2/17
-Project 5, Part 4
+Project 5, Part 3
 */
 #include <iostream>
 
 using namespace std;
-short a = 5, b = 7;
-short bin, counter = 0;
+short bin, counter = 0, i = 0, t, x = 1;
 
-void displayAX() {
-	short x = 1, t;
-	for (int i = 1; i <= 16; i++) {
-		t = bin&x;
-		if (t == 0) {
-			cout << 0;
-		}
-		else {
-			cout << 1;
-		}
-		if ((i % 4) == 0) {
-			cout << " ";
-		}
-		bin = bin >> 1;
+void displayBin() {
+	if (t == 0) {
+		cout << "0";
 	}
+	else {
+		cout << "1";
+	}
+}
 
+void Endline() {
 	cout << endl;
 }
 
-void countAX()  {
-	short x = 1, t;
-	cout << "Elevator will stop at floors no. ";
-
-	for (int i = 1; i <= 16; i++) {
-		t = bin&x;
-		if (t == 1) {
-			cout << i << " ";
-		}
-		bin = bin >> 1;
-	}
-	cout << endl;
+void outputOne() {
+	cout << i << " ";
 }
 
 int main() {
+	//cout << "Elevators will stop at floors no. ";
 	_asm {
-		mov ax, 1001000100001100b;
-		mov bin, ax;
-		call displayAX;
-		mov ax, 1001000100001100b;
-		mov bin, ax;
-		call countAX;
+		mov		ax, 1001000100001100b; //assign random value to ax
+		mov		bin, ax;
+		shl		x, 15;
 
+	displayBinary:
+		mov		ax, bin;
+		shl		bin, 1;
+		and		ax, x;
+		mov		t, ax;
+		call	displayBin;
+		inc		i;
+		cmp		i, 16;
+		jne		displayBinary;
+
+	doneDisplay:
+		mov		ax, 1001000100001100b; //assign random value to ax
+		mov		bin, ax;
+		mov		i, 0;
+
+	findElevators:
+		cmp		i, 16;
+		je		done;
+		mov		ax, bin;
+		shr		bin, 1;
+		and		ax, 1;
+		mov		t, ax;
+		inc		i;
+		cmp		ax, 1;
+		je		displayOne;
+		jmp findElevators;
+
+	displayOne:
+		call outputOne;
+		jmp	findElevators;
+	done:
+		
 	}
 
+	system("pause");
 	return 0;
 }
