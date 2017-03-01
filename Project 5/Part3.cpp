e5f6d4de11c235b5511f2dd64c94e331e590dd71
Gabe Salac <gsalac7@gmail.com>
2017-02-28 20:44:30 -0800

    Update Part3.cpp

--- Project 5/Part3.cpp
+++ Project 5/Part3.cpp
@@ -6,53 +6,7 @@ Project 5, Part 3
 #include <iostream>
 
 using namespace std;
-short bin, counter = 0, i = 0, l;
-
-void countOnes() {
-	short x = 1;
-	short t;
-	for (int i = 1; i <= 16; i++) {
-		t = bin&x;
-		if (t == 1) { //every time we encounter a 1, then increment the counter
-			counter++;
-		}
-		bin = bin >> 1;
-	}
-	cout << counter << " sprinklers are ON" << endl;
-}
-
-void displaySprinklers() {
-	short x = 1;
-	short t;
-	cout << "Defective sprinklers: ";
-	for (int i = 1; i <= 16; i++) {
-		t = bin&x;
-		if (t == 0) { //every time we encounter a 0, then output the index
-			cout << i << " ";
-		}
-		bin = bin >> 1;
-	}
-	cout << endl;
-}
-
-void displayBin() {
-	short x = 1 << 15;
-	short t;
-	for (int i = 1; i <= 16; i++) {
-		t = bin&x;
-		if (t == 0) {
-			cout << 0;
-		}
-		else {
-			cout << 1;
-		}
-		if (i % 4 == 0) {
-			cout << " ";
-		}
-		bin = bin << 1;
-	}
-	cout << endl;
-}
+short bin, counter = 0, i = 0, x = 1, t, j = 16;
 
 void Endline() {
 	cout << endl;
@@ -62,48 +16,86 @@ void outputOne() {
 	cout << i << " ";
 }
 
+void defectiveSprinkler() {
+	cout << "The defective sprinklers are: ";
+}
+
 void display() {
 	cout << counter << " Sprinklers are ON" << endl;
 }
 
+void displayBin() {
+	if (t == 0) {
+		cout << "0";
+	} else {
+		cout << "1";
+	}
+}
+
+void endline() {
+	cout << endl;
+}
+
 int main() {
+	cout << "AX = ";
 	_asm {
-		mov		ax, 0110101000101111b; //assign random value to ax
+		mov		ax, 0x6A2F; //assign random value to ax
 		mov		bin, ax;
+		shl		x, 15;
 
 	displayBinary:
-		cmp		i, 16;
-		je		done;
-		mov		ax, bin;
-		inc		i;
-		shr		bin, 1;
-		and		ax, 1;
-		cmp		ax, 0;
+		mov		ax, bin; //move bin to ax
+		shl		bin, 1; //shift bin left by 1
+		and		ax, x; //check first value of ax
+		mov		t, ax; //display bin of 1 or 0
+		call	displayBin;
+		inc		i; //increment i
+		cmp		i, 16; //stop at 16 bits
+		jne		displayBinary;
+
+		call	endline;
+
+		mov		ax, 0x6A2F; //assign random value to ax
+		mov		bin, ax;
+		mov		i, 16;
+		call	defectiveSprinkler;
+
+	displayDefectiveSprinklers:
+		cmp		i, 0;	//if i == 0 then stop the loop
+		je		done;  //stop loop
+		mov		ax, bin; //move binary to ax
+		shl		bin, 1; //shift the binary left by 1
+		and		ax, x; //find the first value
+		shr		ax, 15; //shift ax right 15 so it becomes 1 or 0
+		cmp		ax, 0; //check if ax is 0, if it is, then output i index
 		je		sprinklers;
-		jmp		displayBinary;
+		dec		i; //decrement i
+		jmp		displayDefectiveSprinklers; //looop again
 	sprinklers:
-		call	outputOne;
-		jmp		displayBinary;
-	
+		call	outputOne; //output i
+		dec		i; //decrement
+		jmp		displayDefectiveSprinklers; //back to loop
+
 	done:
 		call	Endline;
 		mov		i, 0;
-		mov		ax, 0110101000101111b; //assign random value to ax
+		mov		ax, 0x6A2F; //assign random value to ax
 		mov		bin, ax;
 
 	Counter:
-		cmp		i, 16;
-		je		finished;
-		mov		ax, bin;
-		inc		i;
-		shr		bin, 1;
-		and		ax, 1;
-		cmp		ax, 1;
-		je		AddOne;
-		jmp		Counter;
+		cmp		i, 16; //check if i is 0
+		je		finished; //then finish
+		mov		ax, bin; //move bin to ax
+		inc		i; //increment i
+		shr		bin, 1; //shift bin right 1
+		and		ax, 1; //check value of ax
+		cmp		ax, 1; //check if its 1
+		je		AddOne; //if it is add to the counter
+		jmp		Counter; //loop
 	AddOne:
 		inc		counter;
 		jmp		Counter;
+	
 	finished:
 		call	display;
 	}
