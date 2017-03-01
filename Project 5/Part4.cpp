4792674163cd70b0214f6ed448fa6728015133ef
Gabe Salac <gsalac7@gmail.com>
2017-02-28 20:53:04 -0800

    Update Part4.cpp

--- Project 5/Part4.cpp
+++ Project 5/Part4.cpp
@@ -11,8 +11,7 @@ short bin, counter = 0, i = 0, t, x = 1;
 void displayBin() {
 	if (t == 0) {
 		cout << "0";
-	}
-	else {
+	} else {
 		cout << "1";
 	}
 }
@@ -25,45 +24,53 @@ void outputOne() {
 	cout << i << " ";
 }
 
+void elevators() {
+	cout << "Elevator will stop at floors no. ";
+}
+
 int main() {
-	//cout << "Elevators will stop at floors no. ";
+	cout << "AX = ";
 	_asm {
 		mov		ax, 1001000100001100b; //assign random value to ax
 		mov		bin, ax;
 		shl		x, 15;
 
 	displayBinary:
-		mov		ax, bin;
-		shl		bin, 1;
-		and		ax, x;
-		mov		t, ax;
-		call	displayBin;
-		inc		i;
-		cmp		i, 16;
-		jne		displayBinary;
+		mov		ax, bin; //assign bin to ax
+		shl		bin, 1; //shift bin left 1
+		and		ax, x; //compare ax with x
+		mov		t, ax; //move ax to t
+		call	displayBin; //display 1 or 0
+		inc		i; //add 1 to i
+		cmp		i, 16; //check if i is 16 if it is then stop
+		jne		displayBinary; //loop
 
 	doneDisplay:
 		mov		ax, 1001000100001100b; //assign random value to ax
 		mov		bin, ax;
-		mov		i, 0;
+		mov		i, 16; //give i a new value of 16 
+
+		call	Endline;
+		call	elevators;
 
 	findElevators:
-		cmp		i, 16;
-		je		done;
-		mov		ax, bin;
-		shr		bin, 1;
-		and		ax, 1;
-		mov		t, ax;
-		inc		i;
-		cmp		ax, 1;
-		je		displayOne;
-		jmp findElevators;
-
+		cmp		i, 0; //check if i is 0 if it is stop the loop
+		je		done; 
+		mov		ax, bin; //move bin to ax
+		shl		bin, 1; //move bin left 1
+		and		ax, x; //check if ax is 1 or 0 at the left
+		shr		ax, 15; //shift it right 15
+		cmp		ax, 1; //to compare it to 1
+		je		displayOne; //if its 1 then display the index
+		dec		i; //decrement
+		jmp		findElevators; //loop
+ 
 	displayOne:
-		call outputOne;
-		jmp	findElevators;
+		call	outputOne;
+		dec		i;
+		jmp		findElevators;
 	done:
-		
+		call	Endline;
 	}
 
 	system("pause");
