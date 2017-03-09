ee98aab8d5a1774ab3bc2bff7ecffa90a48597a1
Gabe Salac <gsalac7@gmail.com>
2017-03-08 20:45:09 -0800

    Update Part2.cpp

--- Project 6/Part2.cpp
+++ Project 6/Part2.cpp
@@ -1,3 +1,8 @@
+/*Angelo Salac
+Michael Busslinger
+3/9/17
+Project 6 Part 3
+*/
 #include <iostream>
 
 using namespace std;
@@ -6,36 +11,42 @@ int a[5] = { 9, 3, 22, 8, 1 };
 int i = 0, temp;
 
 void display() {
-	cout << temp << " ";
-}
-
-int main() {
-	_asm {
-	set:
-		mov ecx, 0;
-		mov ebx, 0;
-	loop1:
-		cmp i, 25; //do iteration i^2 times
-		je done;
-		inc i; //traverse the array
-		cmp ebx, 16; //once first iteration is done
-		je set; //set value to 0 again
-		mov edx, [a + ebx]; //move first value to eax
-		add ebx, 4; //traverse array with ebx
-		cmp edx, [a + ebx]; // if eax is greater than then swap
-		jge swap; //swap function
-		jmp loop1; //continue
-
-	swap: //got this from wikipedia
-		//mov ecx, [a + ebx];
-		//xor eax, ecx;
-		//xor ecx, eax;
-		//xor eax, ecx;
-		//mov[a + ebx], ecx;
-		jmp loop1;
-
-	done:
-		call display;
+	for (int i = 0; i < 5; i++) {
+		cout << a[i] << " ";
 	}
-	return 0;
+	cout << endl;
+}
+int main() {
+	cout << "Current Array: ";
+	display();
+	_asm {
+		mov edx, 0;
+		lea ecx, a;  //load pointer to esi   
+
+	innerloop:
+		inc i //increment i
+		cmp i, 5 //check if i == 0
+		je outerloop //if it is go to the outer loop
+		mov eax, [ecx] //assign eax the ecx pointer
+		cmp eax, [ecx + 4] //compare eax with next number in array
+		Jng noexchange         //if eax is not greater than, then dont exchange
+		xchg eax, [ecx + 4] //if its true, the exchcange the values
+		mov[ecx], eax //assign eaxy to ecx.
+
+	noexchange :
+		add ecx, 4	//go to next number in array
+		jmp innerloop //then repeat iteration in inner loop
+
+	outerloop :
+		lea ecx, a    //assign pointer to ecx,
+		mov i, 0 //put 0 into i
+		inc edx  //increment edx
+		cmp edx, 5 //check if 5 times since O(n^2) iterations
+		jne innerloop //if not go back to the inner loop
+		mov edx, 0 //set edx back to 0
+		lea ecx, a     //first address of a to ecx
+	}
+	cout << "Sorted Array: ";
+	display();
+	
 }
